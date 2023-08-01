fef324f24657f25606f8b811cc22540fcbfef364
Nwaigba66 <glorianwaigba@gmail.com>
2023-08-01 14:17:58 -0700

    Updated binary_tress.h

--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>git 
+#include <string.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -14,10 +14,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
