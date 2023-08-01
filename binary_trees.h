db7335b7ddbd4ac336cc62d2db847e27faef4532
Nwaigba1234567 <iamkatty3@gmail.com>
2023-08-01 23:15:04 +0200

    updated 0-binary_tree_node.c

--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>git 
+#include <string.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,6 +20,7 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
+void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
