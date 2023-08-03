9129424fb755aa932098b6bd9d825bdce8d94cb0
abubakrali10 <bakrali175@yahoo.com>
2023-08-03 19:44:34 +0300

    binary tree preorder traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - binary tree preorder traversal
+ * @tree: pointer to tree to traverse
+ * @func: user defined binary tree node value printing 
+ *
+ * Return: nothing
+*/
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
